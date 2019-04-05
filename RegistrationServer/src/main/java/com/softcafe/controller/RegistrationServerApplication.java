179432394dad1e1bc71daa2bee39538601ba61e1
DESKTOP-SVIPD1A\DELL <softcafe.ltd@gmail.com>
2019-04-06 01:36:35 +0600

    RUN MICRO SERVICE

---
+++ RegistrationServer/src/main/java/com/softcafe/controller/RegistrationServerApplication.java
@@ -0,0 +1,16 @@
+package com.softcafe.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistrationServerApplication {
+
+  public static void main(String[] args) {
+    // Tell Boot to look for registration-server.yml
+    System.setProperty("spring.config.name", "registration-server");
+    SpringApplication.run(RegistrationServerApplication.class, args);
+  }
+}
